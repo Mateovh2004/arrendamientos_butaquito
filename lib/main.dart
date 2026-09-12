1961ec0e8882a1ad650a8abcea585e3423bc5a9a
Juan David <jdmartinezm@correo.iue.edu.co>
2026-09-12 00:04:18 -0500

    Añadió pantalla de registro de inmuebles

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,57 @@
 import 'package:arrendamientos_butaquito/layout/lista_inmuebles.dart';
+// 1. CORRECCIÓN: Asegúrate de tener esta importación exacta con la ruta de tu archivo
+import 'package:arrendamientos_butaquito/layout/registro/nuevo_inmueble.dart'; 
 import 'package:flutter/material.dart';
-import 'package:arrendamientos_butaquito/layout/menu_inicio.dart';
+
 void main() {
-  runApp( MainApp());
+  runApp(MainApp());
 }
+
 class MainApp extends StatelessWidget {
   MainApp({super.key});
-  TextEditingController controladorCorreo = TextEditingController();
-  TextEditingController controladorContrasena = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          foregroundColor: Colors.white,
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          ],
-        ),
-        body: listadoInmuebles(),
-        //body:widgetMenuInicio(controladorCorreo: controladorCorreo, controladorContrasena: controladorContrasena), // Cierre de AppBar
-      ), // Cierre de Scaffold
-    ); // Cierre de MaterialApp
+      home: PantallaPrincipal(), 
+    );
   }
 }
+
+class PantallaPrincipal extends StatelessWidget {
+  const PantallaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController controladorCorreo = TextEditingController();
+    final TextEditingController controladorContrasena = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue, 
+        foregroundColor: Colors.white,
+        title: const Text('Inmuebles'),
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: const Icon(Icons.search)
+          ),
+          IconButton(
+            onPressed: () {
+              print("Tocaste añadir un nuevo registro");
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PantallaNuevoInmueble()),
+              );
+            }, 
+            icon: const Icon(Icons.add)
+          ),
+        ],
+      ),
+      body: listadoInmuebles(),
+    );
+  }
+}
+
 
